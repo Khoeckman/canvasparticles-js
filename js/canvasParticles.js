696e462e646e09625e20820cedd778445af154ee
Kyle Hoeckman <kyle.hoeckman@student.odisee.be>
2024-12-14 20:31:32 +0100

    Update version to 3.3.3

--- js/canvasParticles.js
+++ js/canvasParticles.js
@@ -2,7 +2,10 @@
 // https://github.com/Khoeckman/canvasParticles/blob/main/LICENSE
 
 class CanvasParticles {
-  static version = '3.2.21'
+  static version = '3.3.3'
+
+  animating = false
+  particles = []
 
   /**
    * Creates a new CanvasParticles instance.
@@ -19,7 +22,6 @@ class CanvasParticles {
     // Get 2d drawing functions
     this.ctx = this.canvas.getContext('2d')
 
-    this.animating = false
     this.formatOptions(options)
 
     // Event handling
@@ -62,7 +64,6 @@ class CanvasParticles {
     if (isNaN(this.options.framesPerUpdate)) this.options.framesPerUpdate = 1
 
     if (isNaN(this.options.mouse.interactionType)) this.options.mouse.interactionType = 1
-    if (isNaN(this.options.mouse.connectDistMult)) this.options.mouse.connectDistMult = 2 / 3
     if (isNaN(this.options.mouse.distRatio)) this.options.mouse.distRatio = 2 / 3
 
     if (isNaN(this.options.particles.ppm)) this.options.particles.ppm = 100
@@ -77,11 +78,8 @@ class CanvasParticles {
     if (isNaN(this.options.gravity.pulling)) this.options.gravity.pulling = 0
     if (isNaN(this.options.gravity.friction)) this.options.gravity.friction = 0.9
 
-    // Transform distance multiplier to absolute distance
-    this.options.mouse.connectDist = this.options.particles.connectDist * this.options.mouse.connectDistMult
-    delete this.options.mouse.connectDistMult
-
     this.setBackground(this.options.background)
+    this.setMouseConnectDistMult(this.options.mouse.connectDistMult)
     this.setParticleColor(this.options.particles.color)
   }
 
@@ -113,10 +111,14 @@ class CanvasParticles {
 
     // Amount of particles to be created
     const particles = Math.floor((this.options.particles.ppm * this.width * this.height) / 1000000)
-    this.len = Math.min(this.options.particles.max, particles)
+    this.particleCount = Math.min(this.options.particles.max, particles)
 
-    if (this.options.resetOnResize || typeof this.particles === 'undefined') this.newParticles()
-    else this.matchParticlesAmount()
+    if (!isFinite(this.particleCount)) throw new RangeError('number of particles must be finite. check `options.particles.ppm`')
+
+    if (this.options.resetOnResize || this.particles.length === 0) this.newParticles()
+    else this.matchParticleCount()
+
+    this.updateParticleBounds()
   }
 
   /**
@@ -124,22 +126,21 @@ class CanvasParticles {
    * The amount of new particles will match 'options.particles.ppm'
    * */
   newParticles = () => {
-    if (this.len === Infinity) throw new RangeError('cannot create an infinite amount of particles')
     this.particles = []
-    for (let i = 0; i < this.len; i++) this.createParticle()
+    for (let i = 0; i < this.particleCount; i++) this.createParticle()
   }
 
   /**
-   * When resizing, add or remove some particles.
-   * The final amount of particles will match 'options.particles.ppm'
+   * When resizing, add or remove some particles so that the final amount of particles will match 'options.particles.ppm'
    * */
-  matchParticlesAmount = () => {
-    if (this.len === Infinity) throw new RangeError('cannot create an infinite amount of particles')
-    this.particles = this.particles.slice(0, this.len)
-    while (this.len > this.particles.length) this.createParticle()
+  matchParticleCount = () => {
+    this.particles = this.particles.slice(0, this.particleCount)
+    while (this.particleCount > this.particles.length) this.createParticle()
   }
 
-  createParticle = function (posX, posY, dir, speed, size) {
+  createParticle = (posX, posY, dir, speed, size) => {
+    size = size || 0.5 + Math.random() ** 5 * 2 * this.options.particles.relSize
+
     this.particles.push({
       posX: posX - this.offX || Math.random() * this.width, // Logical position in pixels
       posY: posY - this.offY || Math.random() * this.height, // Logical position in pixels
@@ -151,17 +152,22 @@ class CanvasParticles {
       offY: 0, // Vertical distance from drawn to logical position in pixels
       dir: dir || Math.random() * 2 * Math.PI, // Direction in radians
       speed: speed || (0.5 + Math.random() * 0.5) * this.options.particles.relSpeed, // Velocity in pixels per update
-      size: size || 0.5 + Math.random() ** 5 * 2 * this.options.particles.relSize, // Ray in pixels of the particle
+      size, // Ray in pixels of the particle
     })
-    const particle = this.particles.at(-1)
+    this.updateParticleBounds()
+  }
 
-    // Within these bounds the particle is considered visible
-    particle.bounds = {
-      top: -particle.size,
-      right: this.canvas.width + particle.size,
-      bottom: this.canvas.height + particle.size,
-      left: -particle.size,
-    }
+  updateParticleBounds = () => {
+    this.particles.map(
+      particle =>
+        // Within these bounds the particle is considered visible
+        (particle.bounds = {
+          top: -particle.size,
+          right: this.canvas.width + particle.size,
+          bottom: this.canvas.height + particle.size,
+          left: -particle.size,
+        })
+    )
   }
 
   /**
@@ -169,7 +175,7 @@ class CanvasParticles {
    * Is executed once every 'options.framesPerUpdate' frames.
    * */
   update = () => {
-    const len = this.len
+    const len = this.particleCount
     const enabledRepulsive = this.options.gravity.repulsive !== 0
     const enabledPulling = this.options.gravity.pulling !== 0
     const gravRepulsiveMult = this.options.particles.connectDist * this.options.gravity.repulsive
@@ -269,7 +275,7 @@ class CanvasParticles {
    * @returns {number} x - The horizontal grid position (0, 1, or 2).
    * @returns {number} y - The vertical grid position (0, 1, or 2).
    */
-  gridPos = function (particle) {
+  gridPos = particle => {
     return {
       x: (particle.x >= particle.bounds.left) + (particle.x > particle.bounds.right),
       y: (particle.y >= particle.bounds.top) + (particle.y > particle.bounds.bottom),
@@ -282,7 +288,7 @@ class CanvasParticles {
    * @param {Object} particleB - Second particle with {gridPos, isVisible}.
    * @returns {boolean} - True if the line crosses the visible center, false otherwise.
    */
-  isLineVisible = function (particleA, particleB) {
+  isLineVisible(particleA, particleB) {
     // Visible if either particle is in the center
     if (particleA.isVisible || particleB.isVisible) return true
 
@@ -318,7 +324,7 @@ class CanvasParticles {
       }
     }
 
-    const len = this.len
+    const len = this.particleCount
     const drawAll = this.options.particles.connectDist >= Math.min(this.canvas.width, this.canvas.height)
 
     const maxWorkPerParticle = this.options.particles.connectDist * this.options.particles.maxWork
@@ -402,6 +408,16 @@ class CanvasParticles {
   }
 
   /**
+   * Transform distance multiplier to absolute distance
+   * @param {float} connectDistMult - The maximum distance for the mouse to interact with the particles.
+   * The value is multiplied by particles.connectDistance
+   * @example 0.8 connectDistMult * 150 particles.connectDistance = 120 pixels
+   */
+  setMouseConnectDistMult = connectDistMult => {
+    this.options.mouse.connectDist = this.options.particles.connectDist * (isNaN(connectDistMult) ? 2 / 3 : connectDistMult)
+  }
+
+  /**
    * Format particle color and opacity
    * @param {string} color - The color of the particles and their connections. Can be any CSS supported color format.
    */
@@ -411,19 +427,20 @@ class CanvasParticles {
     if (this.ctx.fillStyle[0] === '#') {
       this.options.particles.opacity = { value: 255, hex: 'ff' }
       this.options.particles.color = this.ctx.fillStyle
-    } else {
-      // Example: extract 0.25 from rgba(136, 244, 255, 0.25) and convert to range 0x00 to 0xff and store as a 2 char string
-      const value = ~~(this.ctx.fillStyle.split(',').at(-1).slice(1, -1) * 255)
-
-      this.options.particles.opacity = {
-        value: value,
-        hex: value.toString(16),
-      }
-
-      // Example: extract 136, 244 and 255 from rgba(136, 244, 255, 0.25) and convert to '#001122' format
-      this.ctx.fillStyle = this.ctx.fillStyle.split(',').slice(0, -1).join(',') + ', 1)'
-      this.options.particles.color = this.ctx.fillStyle
+      return
     }
+
+    // Example: extract 0.25 from rgba(136, 244, 255, 0.25) and convert to range 0x00 to 0xff and store as a 2 char string
+    const value = ~~(this.ctx.fillStyle.split(',').at(-1).slice(1, -1) * 255)
+
+    this.options.particles.opacity = {
+      value: value,
+      hex: value.toString(16),
+    }
+
+    // Example: extract 136, 244 and 255 from rgba(136, 244, 255, 0.25) and convert to '#001122' format
+    this.ctx.fillStyle = this.ctx.fillStyle.split(',').slice(0, -1).join(',') + ', 1)'
+    this.options.particles.color = this.ctx.fillStyle
   }
 }
 

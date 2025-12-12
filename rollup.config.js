64aa509378671e9b685e55dfecd11f4b99c9db6c
Kyle Hoeckman <kyle.hoeckman@gmail.com>
2025-12-12 23:02:48 +0100

    Change rollup to only use terser with `dist/index.umd.js`

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,5 @@
 import pkg from './package.json' with { type: 'json' }
 import del from 'rollup-plugin-delete'
-import resolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 import replace from '@rollup/plugin-replace'
 import terser from '@rollup/plugin-terser'
@@ -14,28 +12,24 @@ export default {
       format: 'umd',
       name: 'CanvasParticles',
       exports: 'default',
+      plugins: [
+        terser({
+          format: {
+            indent_level: 2,
+            comments: false,
+          },
+        }),
+      ],
     },
     { file: 'dist/index.mjs', format: 'es' },
     { file: 'dist/index.cjs', format: 'cjs' },
   ],
   plugins: [
     del({ targets: 'dist/*' }),
-    resolve({ extensions: ['.ts', '.js', '.json'] }),
     replace({
       preventAssignment: true,
       __VERSION__: JSON.stringify(pkg.version),
     }),
     typescript({ tsconfig: './tsconfig.json' }),
-    commonjs(),
-    terser({
-      maxWorkers: 16,
-      compress: false,
-      mangle: false,
-      format: {
-        beautify: true,
-        indent_level: 2,
-        comments: false,
-      },
-    }),
   ],
 }

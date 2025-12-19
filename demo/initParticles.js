2cb048644d002123c33cc880f8fdeaa2d2109daf
Kyle Hoeckman <kyleh@hybridsoftware.com>
2025-12-19 18:40:52 +0100

    Improve demo

--- demo/initParticles.js
+++ demo/initParticles.js
@@ -5,58 +5,81 @@ new CanvasParticles('#cp-1', {
   background: 'linear-gradient(115deg, #354089, black)',
   mouse: {
     interactionType: 2,
-    connectDistMult: 2,
+    connectDistMult: 200,
     distRatio: 1,
   },
   particles: {
     color: '#88c8ff',
-    // max: 150,
-    // maxWork: 15,
-    connectDistance: 100,
-    max: Infinity,
-    ppm: 1000,
-    relSpeed: 0.8,
-    rotationSpeed: 1,
-  },
-  gravity: {
-    repulsive: 2,
-    friction: 0.8,
+    ppm: 10000,
+    max: 20000,
+    maxWork: 0,
+    connectDistance: 1,
+    relSpeed: 0.05,
   },
 }).start()
 
 // Initialize second canvas
-// new CanvasParticles('#cp-2', {
-//   background: 'hsl(125, 42%, 35%)',
-//   resetOnResize: true,
-//   mouse: {
-//     interactionType: 2,
-//   },
-//   particles: {
-//     color: '#96ff69',
-//     max: 200,
-//     maxWork: 10,
-//   },
-// }).start()
+new CanvasParticles('#cp-2', {
+  background: 'hsl(125, 42%, 35%)',
+  resetOnResize: true,
+  mouse: {
+    interactionType: 2,
+  },
+  particles: {
+    color: '#96ff69',
+    ppm: 250,
+    max: 1000,
+    maxWork: 50,
+    relSpeed: 0.1,
+  },
+}).start()
 
 // Initialize third canvas
-// new CanvasParticles('#cp-3', {
-//   background: '#423',
-//   mouse: {
-//     interactionType: 2,
-//     connectDistMult: 0.5,
-//     distRatio: 0.75,
-//   },
-//   particles: {
-//     color: '#f45c',
-//     ppm: 120,
-//     max: 1200,
-//     maxWork: 100,
-//     connectDistance: 350,
-//     rotationSpeed: 5,
-//   },
-//   gravity: {
-//     repulsive: 10,
-//     pulling: 1,
-//     friction: 0.9,
-//   },
-// }).start()
+new CanvasParticles('#cp-3', {
+  background: '#423',
+  mouse: {
+    interactionType: 2,
+    connectDistMult: 2,
+    distRatio: 0.9,
+  },
+  particles: {
+    color: '#f45c',
+    ppm: 750,
+    max: 1500,
+    maxWork: 50,
+    connectDistance: 100,
+    relSpeed: 1,
+    rotationSpeed: 2,
+  },
+  gravity: {
+    repulsive: 5,
+    pulling: 1,
+    friction: 0.75,
+  },
+}).start()
+
+// Causes issues somehow:
+// Initialize third canvas
+/* new CanvasParticles('#cp-3', {
+  background: '#423',
+  mouse: {
+    interactionType: 2,
+    connectDistMult: 2,
+    distRatio: 0.75,
+  },
+  particles: {
+    color: '#f45c',
+    ppm: 240,
+    max: 1200,
+    maxWork: 100,
+    connectDistance: 50,
+    relSpeed: 0,
+    rotationSpeed: 5,
+  },
+  gravity: {
+    repulsive: 4,
+    pulling: 2,
+    friction: 0.5,
+  },
+}).start()
+ */

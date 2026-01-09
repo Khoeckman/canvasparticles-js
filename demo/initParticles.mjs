c4f7e60374c99c16828dae6f7880b5d9e7caf900
Kyle Hoeckman <kyleh@hybridsoftware.com>
2026-01-09 18:13:28 +0100

    Improve demo code for manual particles

--- demo/initParticles.mjs
+++ demo/initParticles.mjs
@@ -58,29 +58,30 @@ new CanvasParticles('#cp-3', {
 
 const cp4 = new CanvasParticles('#cp-4', {
   mouse: {
-    interactionType: CanvasParticles.interactionType.MOVE, // = 2
+    interactionType: CanvasParticles.interactionType.SHIFT, // = 1
     connectDistMult: 1.25,
     distRatio: 1,
   },
   particles: {
-    generationType: CanvasParticles.generationType.MANUAL, // = 0
+    // generationType: CanvasParticles.generationType.MANUAL, // = 0
     color: 'red',
     maxWork: 100,
+    ppm: 40,
     connectDistance: 200,
     rotationSpeed: 0,
   },
 }).start()
 
-const iw1_4 = window.innerWidth / 4
-const ih1_4 = window.innerHeight / 4
+const w = cp4.width
+const h = cp4.height
 
 // Manually create particles in a sine wave pattern
-for (let x = 0; x < window.innerWidth; x += 4) {
-  const y = ih1_4 + Math.sin(x / 100) * ih1_4
+for (let x = -cp4.offX; x < w; x += 4) {
+  const y = h / 4 + (Math.sin(x / 100) * h) / 4
   cp4.createParticle(x, y, 0, 1, 5)
 }
 
-for (let y = -ih1_4; y < window.innerHeight + cp4.option.particles.connectDist / 2; y += 4) {
-  const x = 2 * iw1_4 + Math.sin(y / 100) * ih1_4
+for (let y = -cp4.offY; y < h; y += 4) {
+  const x = w / 2 + (Math.sin(y / 100) * h) / 4
   cp4.createParticle(x, y, 0, 1, 5)
 }

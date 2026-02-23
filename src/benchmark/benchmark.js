95fcb5ad2f125d0f761055f04b86549d1a213bc6
Kyle Hoeckman <kyle.hoeckman@gmail.com>
2026-02-23 07:04:14 +0100

    Update to 4.4.10

--- src/benchmark/benchmark.js
+++ src/benchmark/benchmark.js
@@ -1,4 +1,4 @@
-import CanvasParticles from 'https://cdn.jsdelivr.net/npm/canvasparticles-js@4.4.9/dist/index.mjs'
+import CanvasParticles from 'https://cdn.jsdelivr.net/npm/canvasparticles-js@4.4.10/dist/index.mjs'
 
 // Version
 document.getElementById('version').innerText = CanvasParticles.version ?? 'unknown'
